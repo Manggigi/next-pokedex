c20e17e7b7455d26784242bd5f775e03de19dfdd
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-04-30 18:06:56 +0800

    feat: created route

---
+++ utils/routes.ts
@@ -0,0 +1,19 @@
+const routes = {
+  home: "/",
+  pokedex: "/pokedex",
+  type: "/type",
+  moves: "/moves",
+  items: "/items",
+  locations: "/locations",
+  abilities: "/abilities",
+  pokemon: { link: (pokemonId: string) => `/pokemon/${pokemonId}` },
+  profile: {
+    index: "/profile",
+    user: {
+      link: (userId: string) => `/profile/${userId}`,
+      href: "/profile/[userLink]",
+    },
+  },
+};
+
+export default routes;
