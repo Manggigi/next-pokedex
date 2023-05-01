d19315e3285868be77e472be7ba3340b5140829c
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-05-01 17:55:02 +0800

    feat: update components for pokedex

--- components/GridList/Cards.tsx
+++ components/GridList/Cards.tsx
@@ -4,171 +4,62 @@ import Image from "next/image";
 import Link from "next/link";
 import routes from "@component/utils/routes";
 import Divider from "../Divider";
+const cleanPokemonsData = require("../../tools/cleanPokemonsData.json");
 
-const people = [
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/5.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/19.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/18.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/17.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/16.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/15.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/14.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/6.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/8.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/13.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/9.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg",
-  },
-  {
-    name: "Bulbasaur",
-    title: "Paradigm Representative",
-    role: "Admin",
-    email: "janecooper@example.com",
-    telephone: "+1-202-555-0170",
-    imageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/12.svg",
-  },
-  // More people...
-];
+export type Pokemon = {
+  abilities: string[];
+  moves: string[];
+  types: PokemonType[];
+  stats: {
+    statName: string;
+    baseStat: number;
+  }[];
+  baseExperience: number;
+  id: number;
+  isDefault: boolean;
+  name: string;
+  pictureUrl: string;
+};
+
+export enum PokemonType {
+  Grass = "grass",
+  Poison = "poison",
+  Fire = "fire",
+  Water = "water",
+  Bug = "bug",
+  Normal = "normal",
+  Flying = "flying",
+  Electric = "electric",
+  Ground = "ground",
+  Psychic = "psychic",
+  Fighting = "fighting",
+  Steel = "steel",
+  Rock = "rock",
+  Fairy = "fairy",
+  Ice = "ice",
+  Ghost = "ghost",
+}
+
+const pokemons = cleanPokemonsData.filter(
+  (pokemon: Pokemon) => pokemon.id < 100
+) as Pokemon[];
+
+console.log(pokemons.map((p) => p.types));
 
 export default function Cards() {
   return (
     <ul role="list" className="flex flex-wrap justify-center align-middle">
-      {people.map((person) => (
+      {pokemons.map((pokemon: Pokemon) => (
         <li
-          key={person.email}
-          className="relative col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow m-2 h-64"
+          key={pokemon.id}
+          className="relative col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-slate-200 text-center shadow m-2 h-60"
         >
-          <Link href={routes.pokemon.link("1")}>
-            <div className="p-8 relative">
-              <Badge color="gray" className="top-3 left-3 absolute">
-                #01
-              </Badge>
+          <Link href={routes.pokemon.link(pokemon.id)}>
+            <div className="h-16 px-8 mt-8 ">
+              {/* <Badge className="top-3 left-3 absolute">#{pokemon.id}</Badge> */}
               <Image
-                className="mx-auto flex-shrink-0 object-contain"
-                src={person.imageUrl}
+                className="mx-auto flex-shrink-0 object-contain max-h-32"
+                src={pokemon.pictureUrl}
                 alt="pokemon image"
                 width={120}
                 height={120}
@@ -176,17 +67,16 @@ export default function Cards() {
             </div>
 
             <div className="absolute bottom-1 -mt-px flex divide-x divide-gray-200 ">
-              <div className="flex w-0 flex-1 py-2">
-                <div className="w-0 rounded-bl-lg border border-transparent text-sm font-semibold text-gray-900 pl-3">
-                  {person.name}
-                  <div className="flex">
-                    <Badge color="green" bgColor="green" className="mr-1">
-                      grass
-                    </Badge>
-                    <Badge color="blue" bgColor="blue">
-                      water
-                    </Badge>
-                  </div>
+              <div className="flex w-0 flex-1 py-2 ">
+                <div className="w-0 rounded-bl-lg border border-transparent text-sm font-semibold text-gray-700 pl-3">
+                  {pokemon.name}
+                  {/* <div className="flex mt-2">
+                    {pokemon.types.map((type) => (
+                      <Badge type={type} key={type} className="mr-1">
+                        {type}
+                      </Badge>
+                    ))}
+                  </div> */}
                 </div>
               </div>
             </div>
