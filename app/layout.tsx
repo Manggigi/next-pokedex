7ba922395f1ce1af1cf2ddec830039553de85bdf
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-05-03 15:08:16 +0800

    Revert "Develop"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "PokeGee",
-  description: "Pokedex created by Gian Denorte",
+  title: "Pokedex by Pokemon API",
+  description: "Created by Gian Denorte",
 };
 
 export default function RootLayout({
@@ -16,9 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <Providers> */}
       <body className={inter.className}>{children}</body>
-      {/* </Providers> */}
     </html>
   );
 }
